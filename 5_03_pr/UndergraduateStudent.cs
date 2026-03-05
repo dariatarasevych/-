5f327153f80b98159c0f14a5abc9ece576b51d79
daratarasevic <tarasevish80@gmail.com>
2026-03-05 19:15:07 +0200

    update UndergraduateStudent

--- 5_03_pr/UndergraduateStudent.cs
+++ 5_03_pr/UndergraduateStudent.cs
@@ -1,6 +1,12 @@
 namespace _5_03_pr;
 
-public class UndergraduateStudent
+public class UndergraduateStudent : UniversityMember
 {
-    
+    public UndergraduateStudent(string name, string memberId) : base(name, memberId) {}
+
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        Console.WriteLine("Lab work completed");
+    }
 }
