71bf626bbe1224c7a8bc2b313e0be7565db51cb7
daratarasevic <tarasevish80@gmail.com>
2026-03-05 19:34:10 +0200

    update UniversityRegistry

--- 5_03_pr/UniversityRegistry.cs
+++ 5_03_pr/UniversityRegistry.cs
@@ -2,5 +2,23 @@ namespace _5_03_pr;
 
 public class UniversityRegistry
 {
-    
+    private List<UniversityMember> _members = new List<UniversityMember>();
+
+    public void AddMember(UniversityMember m) => _members.Add(m);
+
+    public void ExecuteAllDuties()
+    {
+        foreach (var member in _members)
+        {
+            member.PerformDuties();
+        }
+    }
+
+    public string GetMemberStatistics()
+    {
+        foreach (var member in _members)
+        {
+            
+        }
+    }
 }
