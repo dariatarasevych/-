e3b504d032f491188929f94d860815b7b4a78efa
daratarasevic <tarasevish80@gmail.com>
2026-03-05 19:19:20 +0200

    update GraduateStudent

--- 5_03_pr/GraduateStudent.cs
+++ 5_03_pr/GraduateStudent.cs
@@ -1,6 +1,11 @@
 namespace _5_03_pr;
 
-public class GraduateStudent
+public class GraduateStudent : UndergraduateStudent
 {
-    
+    public GraduateStudent (string name, string memberId) : base(name, memberId) {}
+    public override void PerformDuties()
+    {
+        base.PerformDuties();
+        Console.WriteLine("Thesis research update");
+    }
 }
