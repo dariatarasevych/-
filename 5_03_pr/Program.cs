885ba775508f82bb50224974250459b24c852a8a
daratarasevic <tarasevish80@gmail.com>
2026-03-05 19:54:43 +0200

    Tested Program

--- 5_03_pr/Program.cs
+++ 5_03_pr/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+        var registry = new UniversityRegistry();
         
+        registry.AddMember(new Professor("Tomas J.", "pr1"));
+        registry.AddMember(new UndergraduateStudent("Anna T.", "st1"));
+        registry.AddMember(new GraduateStudent("Mike R.", "stg2"));
+        
+        Console.WriteLine("Chek:");
+        registry.ExecuteAllDuties();
+        
+        Console.WriteLine("Statistic");
+        registry.GetMemberStatistics();
+
     }
 }
