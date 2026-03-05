579fc7f66eac5693c81a9f998399769f8dbc78dc
daratarasevic <tarasevish80@gmail.com>
2026-03-05 18:56:53 +0200

    update UniversityMember

--- 5_03_pr/UniversityMember.cs
+++ 5_03_pr/UniversityMember.cs
@@ -2,5 +2,26 @@ namespace _5_03_pr;
 
 public class UniversityMember
 {
+    private string _name;
+
+    public string Name
+    {
+        get => _name;
+        set => _name = string.IsNullOrEmpty(value) ? throw new Exception("Invalid name") : value;
+    }
     
+    public string MemberId { get;  }
+
+    protected List<string> ActionLog = new List<string>();
+
+    public UniversityMember(string name, string memberId)
+    {
+        _name = name;
+        MemberId = memberId;
+    }
+
+    public virtual void PerformDuties()
+    {
+        
+    }
 }
